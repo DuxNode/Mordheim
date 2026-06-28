88a21283f35255f34e9aadae1499a5828d2f6b6b
DuxNode <ash@ashlaws.net>
2026-06-28 21:53:02 +0200

    Add full favicon set

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,6 +14,35 @@ const config: QuartzConfig = {
     analytics: null,
     locale: "en-GB",
     baseUrl: "wyrdstone.wiki",
+    favicon: "/static/favicon.ico",
+    head: [
+      {
+        tag: "link",
+        attrs: {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/static/apple-touch-icon.png",
+        },
+      },
+      {
+        tag: "link",
+        attrs: {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/static/favicon-32x32.png",
+        },
+      },
+      {
+        tag: "link",
+        attrs: {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/static/favicon-16x16.png",
+        },
+      },
+    ],
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
